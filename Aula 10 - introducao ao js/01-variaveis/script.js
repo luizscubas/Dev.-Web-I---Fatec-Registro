65ab6cb979157bc7aecbb5f8cd521fd1763de7e3
Luiz Cubas <lhs.cubas@gmail.com>
2026-05-19 22:44:32 -0300

    fechamneto aula

--- Aula 10 - introducao ao js/01-variaveis/script.js
+++ Aula 10 - introducao ao js/01-variaveis/script.js
@@ -7,20 +7,32 @@ VAR, LET, CONST*/
 var nome = "Luiz";
 var numero = 2;
 
-
 //let:
-let cidade = "registro"
-cidade = "Sete Barras"
+let cidade = "registro";
+cidade = "Sete Barras";
 
 //Const:
-const user = "luiz.cubas@gmail.com"
-//user = "luiz.cubas@gmail.com" -> não pode 
-
+const user = "luiz.cubas@gmail.com";
+//user = "luiz.cubas@gmail.com" -> não pode
 
 //funções do elemento window
 
 //disparando uma janela de alerta no site
-window.alert("Bem-vindo ao meu site")
+window.alert("Bem-vindo ao meu site");
 
-//disparando uma janela de confirmação 
-window.confirm("")
+//disparando uma janela de confirmação
+const confirmacao = window.confirm("Vamos começar?");
+
+
+if (confirmacao == true) {
+  //dispara uma janela de entrada de dados
+  const nome = window.prompt("Qual é o seu nome?");
+  const idade = window.prompt("Quantos anos você tem?")
+
+  //escrevendo no site
+  document.writeln("Olá," + nome); // concatenação
+
+  // concatenação - Template Strings
+  // document.write("Você tem" + idade + "anos")
+  document.writeln(`Você tem ${idade}`)
+}
