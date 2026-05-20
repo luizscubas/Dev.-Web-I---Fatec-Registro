334988cdc4e695fd19ba6c6872ea597c364c5c5d
Luiz Cubas <lhs.cubas@gmail.com>
2026-05-19 22:26:45 -0300

    segunda aula javascript

---
+++ Aula 10 - introducao ao js/01-variaveis/script.js
@@ -0,0 +1,26 @@
+// javaScript
+// VÁRIAVEIS
+/* Variáveis podem ser declaradas de 3 formas:
+VAR, LET, CONST*/
+
+//var:
+var nome = "Luiz";
+var numero = 2;
+
+
+//let:
+let cidade = "registro"
+cidade = "Sete Barras"
+
+//Const:
+const user = "luiz.cubas@gmail.com"
+//user = "luiz.cubas@gmail.com" -> não pode 
+
+
+//funções do elemento window
+
+//disparando uma janela de alerta no site
+window.alert("Bem-vindo ao meu site")
+
+//disparando uma janela de confirmação 
+window.confirm("")
