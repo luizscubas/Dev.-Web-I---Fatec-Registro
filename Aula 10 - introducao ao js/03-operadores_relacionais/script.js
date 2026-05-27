799dad6887902a68796dad8812b6559b38ae52c6
Luiz Cubas <lhs.cubas@gmail.com>
2026-05-26 22:21:20 -0300

    .

--- Aula 10 - introducao ao js/03-operadores_relacionais/script.js
+++ Aula 10 - introducao ao js/03-operadores_relacionais/script.js
@@ -1,3 +1,9 @@
-const nome = window.prompt(`Qual é o seu nome?`)
-const idade = Number(window.prompt(`Qual é a sua idade?`))
-const hobby = window.prompt(`Qual é o seu hobby?`)
+const nome = window.prompt(`Qual é o seu nome?`);
+const idade = Number(window.prompt(`Qual é a sua idade?`));
+const hobby = window.prompt(`Qual é o seu hobby?`);
+
+document.writeln(`Olá ${nome}!`);
+
+// Operador ternário
+const resultado = idade < 18 ? 'menor' : 'maior'
+document.writeln(`você é <strong>${resultado}<strong> de idade.`);
