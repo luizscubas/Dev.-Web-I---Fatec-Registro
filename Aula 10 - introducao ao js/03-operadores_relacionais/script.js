b5410ac11f1ed84bdddb3d633461d443310f5cd0
Luiz Cubas <lhs.cubas@gmail.com>
2026-05-26 22:48:25 -0300

    .

--- Aula 10 - introducao ao js/03-operadores_relacionais/script.js
+++ Aula 10 - introducao ao js/03-operadores_relacionais/script.js
@@ -1,9 +1,21 @@
 const nome = window.prompt(`Qual é o seu nome?`);
 const idade = Number(window.prompt(`Qual é a sua idade?`));
 const hobby = window.prompt(`Qual é o seu hobby?`);
+const sexo = window.prompt(
+  `Qual é o seu sexo? Digte M (Masculino) F (Feminino)`,
+);
 
 document.writeln(`Olá ${nome}!`);
 
 // Operador ternário
-const resultado = idade < 18 ? 'menor' : 'maior'
+let resultado = idade < 18 ? "menor" : "maior";
 document.writeln(`você é <strong>${resultado}<strong> de idade.`);
+
+resultado =
+  hobby.toLowerCase() != "estudar"
+    ? "legal eim"
+    : "parabéns você será um ótimo uber";
+document.writeln(`<br> Seu hobby é ${hobby}. ${resultado}`);
+
+resultado = sexo.toUpperCase() == "M" ? "o" : "a";
+document.writeln(`<br> Prazer em conhece-l${resultado}`);
